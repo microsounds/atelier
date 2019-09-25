3a8f23bc3285513e87b176eddcc6d2b03e23c830
microsounds <microsounds@users.noreply.github.com>
2019-09-25 13:08:39 -0700

    dwm default keybindings

--- home/microsounds/.config/dwm/config.h
+++ home/microsounds/.config/dwm/config.h
@@ -1,20 +1,17 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx = 1;	/* window border width */
-static const unsigned int snap = 16;	/* snap pixel */
-static const int showbar = 1;			/* 0 means no bar */
-static const int topbar = 1;			/* 0 means bottom bar */
-static const char *fonts[] = {
-	"Liberation Serif:size=12",
-	"Dejima:size=12" /* japanese serifs */
-};
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 16;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[] = { "Liberation Serif:size=12", "Dejima:size=12" };
 static const char dmenufont[] = "Liberation Serif:size=12";
 static const char col_gray1[] = "#272727";
 static const char col_gray2[] = "#646464";
 static const char col_gray3[] = "#FFFFFF";
 static const char col_gray4[] = "#FFFFFF"; /* fg text */
-static const char col_cyan[]  = "#B24A7A"; /* CDE salmon pink~ */
+static const char col_cyan[]  = "#B24A7A"; /* CDE salmon pink */
 static const char *colors[][3]= {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -39,7 +36,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* ignore column limitations in terminals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -48,69 +45,70 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+/* key definitions */
+#define CTRL ControlMask
+#define ALT Mod1Mask
+#define META Mod4Mask
+#define SHIFT ShiftMask
+#define MODKEY ALT /* primary modkey */
+
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
 /* built-in commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvtc", NULL };
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
-/* volume functions */
+/* volume control */
 static const char *vol_up[] = { "amixer", "-q", "sset", "Master", "5%+", NULL };
 static const char *vol_dn[] = { "amixer", "-q", "sset", "Master", "5%-", NULL };
 static const char *vol_mute[] = { "amixer", "-q", "-D", "pulse", "sset", "Master", "toggle", NULL };
 
-/* modifier keys */
-#define SHIFT ShiftMask
-#define CTRL ControlMask
-#define META Mod4Mask
-#define ALT Mod1Mask
-
-#define TAGKEYS(KEY, TAG) \
-	{ META,					KEY,	view,           { .ui = 1 << TAG } }, \
-	{ CTRL | META,			KEY,	toggleview,     { .ui = 1 << TAG } }, \
-	{ META | SHIFT,			KEY,	tag,            { .ui = 1 << TAG } }, \
-	{ CTRL | META | SHIFT,	KEY,	toggletag,      { .ui = 1 << TAG } },
-
 static Key keys[] = {
-	/* modifiers	+ key		action		args */
-	{ META,			XK_p,		spawn,			{ .v = dmenucmd } },	/* dmenu */
-	{ META,			XK_Return,	spawn,			{ .v = termcmd } },		/* termi */
-	{ ALT,			XK_F4,		killclient,		{0} },					/* ALT+F4 */
-	{ CTRL | SHIFT, XK_q,		quit,			{0} },					/* exit dwm */
-	{ META,			XK_b,		togglebar,		{0} },					/* hide bar */
-	{ META,			XK_Tab,		view,			{0} },					/* cycle tags */
-	{ ALT,			XK_Tab,		focusstack,		{ .i = +1 } },			/* cycle forward */
-	{ ALT | SHIFT,	XK_Tab,		focusstack,		{ .i = -1 } },			/* cycle backward */
-	{ META,			XK_equal,	incnmaster,		{ .i = +1 } },			/* +++ windows in master area */
-	{ META,			XK_minus,	incnmaster,		{ .i = -1 } },			/* --- windows in master area */
-	{ META,			XK_Left,	setmfact,		{ .f = -0.05 } },		/* +++ master area size */
-	{ META,			XK_Right,	setmfact,		{ .f = +0.05 } },		/* --- master area size */
-	{ ALT,			XK_Return,	zoom,			{0} },					/* promote to master area */
-	{ META,			XK_t,		setlayout,		{.v = &layouts[0]} },	/* tiling mode */
-	{ META,			XK_f,		setlayout,		{.v = &layouts[1]} },	/* floating mode */
-	{ META,			XK_m,		setlayout,		{.v = &layouts[2]} },	/* monocle mode */
-	{ META,			XK_space,	setlayout,		{0} },					/* cycle modes */
-	{ META | SHIFT, XK_space,	togglefloating, {0} },					/* cycle modes for current master */
-	{ META,			XK_0,		view,			{ .ui = ~0 } },			/* select all tags */
-	{ META | SHIFT,	XK_0,		tag,			{ .ui = ~0 } },			/* move to all tags */
-	{ META,			XK_comma,	focusmon,		{ .i = -1 } },
-	{ META,			XK_period,	focusmon,		{ .i = +1 } },
-	{ META | SHIFT,	XK_comma,	tagmon,			{ .i = -1 } },
-	{ META | SHIFT,	XK_period,	tagmon,			{ .i = +1 } },
-	{ 0,			XF86XK_AudioRaiseVolume,	spawn, { .v = vol_up } },	/* vol up */
-	{ 0,			XF86XK_AudioLowerVolume,	spawn, { .v = vol_dn } },	/* vol down */
-	{ 0,			XF86XK_AudioMute,			spawn, { .v = vol_mute } }, /* mute */
-	TAGKEYS(XK_1, 0)
-	TAGKEYS(XK_2, 1)
-	TAGKEYS(XK_3, 2)
-	TAGKEYS(XK_4, 3)
-	TAGKEYS(XK_5, 4)
-	TAGKEYS(XK_6, 5)
-	TAGKEYS(XK_7, 6)
-	TAGKEYS(XK_8, 7)
-	TAGKEYS(XK_9, 8)
+	/* modifier                     key        function        argument */
+	{ 0,              XF86XK_AudioRaiseVolume, spawn,          {.v = vol_up } },
+	{ 0,              XF86XK_AudioLowerVolume, spawn,          {.v = vol_dn } },
+	{ 0,              XF86XK_AudioMute,        spawn,          {.v = vol_mute } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
@@ -121,12 +119,12 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         META,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         META,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         META,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            META,         Button1,        tag,            {0} },
-	{ ClkTagBar,            META,         Button3,        toggletag,      {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
