6f167a1c51cc0c4f67b34b1aa6b36542031e08ed
microsounds <microsounds@users.noreply.github.com>
2019-12-30 20:21:38 -0800

    Rewrote to use git-status porcelain version 1

--- Scripts/git_status.sh
+++ Scripts/git_status.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# git_status.sh v0.3
+# git_status.sh v0.4
 # returns repo name, ⎇ branch name, and status indicators in the form '±repo:branch*'
 # pass '-e' to double-escape ANSI color codes
 # bash ignores escape codes when calculating $PS1 length
@@ -12,30 +12,35 @@ dirty='\e[1;31m'
 alt='\e[1;36m'
 reset='\e[0m'
 
-data="$(git status -b --porcelain=v2 2>&1)"
+data="$(git status -b --porcelain 2>&1)"
 if [ $? -eq 0 ]; then
 	# repo name
 	repo="$(git rev-parse --show-toplevel)"
 	# branch info
-	info="$(echo "$data" | grep '^#' | cut -d ' ' -f3-)"
-	for f in $(echo "$info" | sed -n 2p); do
-		case $f in # use commit hash for detached head
-			\(detached\)) branch="$(echo "$info" | sed -n 1p | cut -c -7)";;
-			*) branch="$f";;
+	info="$(echo "$data" | head -1 | \
+	        sed -e 's/ahead /+/' -e 's/behind /-/' | tr '#.,[] ' '\n' | grep .)"
+	for f in $(echo "$info" | head -1); do
+		case $f in
+			HEAD)
+				branch="$(cut -c -7 "$repo/.git/HEAD")";; # detached head mode
+			*)
+				branch="$f" # normal mode, get upstream if exists
+				if [ $(echo "$info" | wc -l) -ge 3 ]; then
+					# ignores stupid branch names (eg. '+1')
+					for g in $(echo "$info" | tail -n +3); do
+						case $g in \+* | -*) ups="$ups $g";; esac
+					done
+				fi;;
 		esac
 	done
-	# upstream info
-	for f in $(echo "$info" | sed -n 4p); do # filter out +0 -0
-		case $f in +0);; -0);; *) ups="$ups $f";; esac
-	done
-	# determine state
+	# index/worktree state
 	color="$clean" # default
-	state="$(echo "$data" | grep -v '^#' | cut -d ' ' -f2)"
-	[ ! -z "$(echo "$state" | cut -c 1 | tr -d '.')" ] && color="$staged" && bits="$bits+"
-	[ ! -z "$(echo "$state" | cut -c 2 | tr -d '.')" ] && color="$dirty" && bits="$bits*"
-	[ ! -z "$(echo "$state" | grep '^?')" ] && color="$dirty" && bits="$bits%" # untracked
-	[ ! -z "$(echo "$state" | grep '^u')" ] && branch="<!>$branch" # merge conflict
-	[ -f "$repo/.git/refs/stash" ] && bits="^$bits"
+	state="$(echo "$data" | tail -n +2 | cut -c -2)"
+	[ ! -z "$(echo "$state" | cut -c 1 | tr -d '? ')" ] && color="$staged" && bits="$bits+"
+	[ ! -z "$(echo "$state" | cut -c 2 | tr -d '? ')" ] && color="$dirty" && bits="$bits*"
+	echo "$state" | grep -q '?' && color="$dirty" && bits="$bits%" # untracked files
+	echo "$state" | grep -q 'U' && branch="<!>$branch" # merge conflict
+	[ -f "$repo/.git/refs/stash" ] && bits="^$bits" # stash exists
 	if [ "$1" = '-e' ]; then color="\[$color\]"; alt="\[$alt\]"; reset="\[$reset\]"; fi
 	printf '%b' "$color±${repo##*/}:$branch$bits$alt$ups$reset"
 fi
