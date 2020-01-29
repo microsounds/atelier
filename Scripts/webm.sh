751a2bc1492cda7e15fc087b3e122c184ace8209
microsounds <microsounds@users.noreply.github.com>
2020-01-29 15:02:50 -0800

    webm.sh: Quitting cleanly upon Ctrl+C

--- Scripts/webm.sh
+++ Scripts/webm.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
-## webm.sh v0.2
-## Press Ctrl+C to end recording.
+## webm.sh v0.3
+## Press Ctrl+C to end recording, press Ctrl+C again to cancel.
+
+# colors
+INFO=37
+OK=32
+ERR=31
 
 # quality
 SIZE=3M
@@ -10,17 +15,29 @@ SCALE=-1:-1 # 800:-1
 FPS=60 # 25
 
 # system
-CORES=$(grep -c 'proc' /proc/cpuinfo)
+CORES=$(grep -c '^proc' /proc/cpuinfo)
 RES=$(xdpyinfo | grep 'dim' | egrep -o '([0-9]+x?)+' | sed -n 1p)
-TEMP=$(mktemp -u --suffix=".mp4")
-FINAL="Screenshot - $(date '+%m%d%Y') - $(date '+%r').webm"
+FINAL="$(date '+Screenshot - %m%d%Y - %r').webm"
+TEMP="/tmp/$(cat /dev/urandom | tr -cd 'a-z0-9' | head -c 10).mp4"
+
+info() { echo "\e[1;${1}m${2}\e[0m"; }
 
 to_webm() {
-	ffmpeg -i "$TEMP" -c:v libvpx -b:v $BITRATE -fs $SIZE -vf scale=$SCALE -threads $CORES -an "$FINAL"
-	rm -v $TEMP
-	echo "File created at: $(readlink -f "$FINAL")"
+	trap really_quit 2
+	info $INFO "Encoding..."
+	ffmpeg -hide_banner -i "$TEMP" \
+	       -c:v libvpx -b:v $BITRATE -fs $SIZE -vf scale=$SCALE -threads $CORES -an "$FINAL"
+	rm -v "$TEMP"
+	info $OK "File saved at: $PWD/$FINAL"
 }
 
-cat $0 | grep '^##' | sed 's/## //g'
+really_quit() {
+	info $ERR "Terminated abruptly..."
+	rm -v "$TEMP" "$FINAL"
+	exit 1
+}
+
+info $INFO "$(cat $0 | grep '^##' | sed 's/## //g')"
 trap to_webm 2
-ffmpeg -loglevel panic -threads $CORES -framerate $FPS -video_size $RES -f x11grab -i :0.0+0,0 -vcodec libx264 -qp 0 -preset ultrafast "$TEMP"
+ffmpeg -loglevel panic -threads $CORES -framerate $FPS -video_size $RES \
+       -f x11grab -i :0.0+0,0 -vcodec libx264 -qp 0 -preset ultrafast "$TEMP"
