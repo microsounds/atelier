98dacf6f81734fb07e775232ae86daa0621f1451
microsounds <microsounds@users.noreply.github.com>
2020-02-20 20:26:27 -0800

    dwm: Install to ~/.local/bin

--- .once.d/00-dwm-install.sh
+++ .once.d/00-dwm-install.sh
@@ -9,9 +9,10 @@ cd ~/.config/dwm
 git init
 git remote add origin 'https://git.suckless.org/dwm'
 if git fetch --tags origin master; then
-	git checkout "$VER"
-	for f in patches/*; do # do not undo patches
-		patch -N < $f; rm -f *.rej
+	git checkout -f "$VER"
+	for f in patches/*; do # apply patches
+		patch < $f;
 	done
-	sudo make install -j $(grep -c 'proc' /proc/cpuinfo)
+	make PREFIX="$HOME/.local" \
+	     install -j $(grep -c '^proc' /proc/cpuinfo)
 fi
