f5bbd160b10979efde19df010234dcdc0e6f2ec0
microsounds <microsounds@users.noreply.github.com>
2020-03-01 00:33:36 -0800

    Extract plugin directly to temp folder in one step

--- .once.d/99-chromium-widevine.sh
+++ .once.d/99-chromium-widevine.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-# chromium_widevine.sh v0.1
-# Downloads and extracts proprietary Widevine content decryption module from Google Chrome
+# chromium_widevine.sh v0.2
+# Downloads and extracts proprietary Widevine content decryption plugin from Google Chrome
 # into your existing Chromium installation to enable use of DRM restricted streaming services.
 
-TMP="/tmp/$(cat /dev/urandom | tr -cd 'a-z0-9' | head -c 7)"
+TMP="/tmp/$(tr -cd 'a-z0-9' < /dev/urandom | head -c 7)"
 SOURCE='https://dl.google.com/linux/direct'
 DEB='google-chrome-stable_current_amd64.deb'
 DATA='data.tar.xz'
 EXTRACT='opt/google/chrome/WidevineCdm'
 
 # default install location
-INSTALL='/usr/lib/chromium'
+TARGET="/usr/lib/chromium/${EXTRACT##*/}"
 
 finish() {
 	rm -rv "$TMP"
@@ -23,10 +23,9 @@ trap finish 2
 
 echo "$0"
 mkdir -v "$TMP"
-cd "$TMP"
 if wget "$SOURCE/$DEB" -O "$TMP/$DEB"; then
-	ar -xv "$DEB" "$DATA"
-	tar -xJvf "$DATA"  "./$EXTRACT"
-	sudo cp -rv "$TMP/$EXTRACT" "$INSTALL"
+	ar -p "$TMP/$DEB" "$DATA" | tar -C "$TMP" -xJv "./$EXTRACT"
+	sudo rm -rf "$TARGET"
+	sudo mv -v "$TMP/$EXTRACT" "$TARGET"
 fi
 finish
