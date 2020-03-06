c0fa3de33087a2423d8d30c956a3985db07d49f8
microsounds <microsounds@users.noreply.github.com>
2020-03-05 21:44:44 -0800

    xrandr_cycle: zero indexing

--- Scripts/xrandr_cycle.sh
+++ Scripts/xrandr_cycle.sh
@@ -5,8 +5,7 @@
 
 get_field() { tr ' ' '\t' < '/dev/stdin' | cut -f$1; }
 mark_array() { tr '\n' ' ' < '/dev/stdin' | sed "s/$1/\*&/g"; }
-
-DISPLAYS="$(xrandr -q | egrep '[^dis]connected')"
+DISPLAYS="$(xrandr -q | grep '[^dis]connected')"
 PRIMARY="$(echo "$DISPLAYS" | fgrep 'primary' | get_field 1)"
 DISPLAYS="$(echo "$DISPLAYS" | get_field 1)"
 
@@ -16,17 +15,15 @@ if [ -z "$PRIMARY" ]; then
 fi
 
 echo "Displays found: $(echo "$DISPLAYS" | mark_array "$PRIMARY")"
-for f in $PRIMARY; do
-	i=0; for g in $DISPLAYS; do
-		if echo "$g" | grep -q "$f"; then
-			SELECT="$((((i + 1) % $(echo "$DISPLAYS" | wc -l)) + 1))"
-			break
-		fi
-		i=$((i + 1))
-	done
+i=0; for f in $DISPLAYS; do
+	if [ "$f" = "$PRIMARY" ]; then
+		SELECT=$(((i + 1) % $(echo "$DISPLAYS" | wc -l)))
+		break
+	fi
+	i=$((i + 1))
 done
 
-SELECT="$(echo "$DISPLAYS" | tail -n +$SELECT | head -1)"
+SELECT="$(echo "$DISPLAYS" | tail -n +$((SELECT + 1)) | head -1)"
 echo "Switching to $SELECT."
 xrandr --output "$SELECT" --primary --auto
 [ "$SELECT" != "$PRIMARY" ] && xrandr --output "$PRIMARY" --off
