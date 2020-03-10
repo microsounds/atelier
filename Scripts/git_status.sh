25e97765b1ad25876e6fc6fc5b688eb11c0040d9
microsounds <microsounds@users.noreply.github.com>
2020-03-10 16:23:09 -0700

    git_status: support for git submodules

--- Scripts/git_status.sh
+++ Scripts/git_status.sh
@@ -15,21 +15,25 @@ reset='\e[0m'
 # is this a git repo?
 data="$(git status -b --porcelain 2> /dev/null)" || exit
 
-# repo/branch info
+# repo location
 repo="$(git rev-parse --show-toplevel)"
+git_dir="$repo/.git" # is this a submodule?
+[ ! -f "$git_dir" ] || read _ git_dir < "$git_dir"
+
+# branch info
 info="$(echo "$data" | head -1 | tr -s '#.,[]() ' '\n' | grep '.')"
 for f in $(echo "$info" | head -1); do
 	case $f in
 		HEAD) # detached HEAD mode
-			branch="$(cut -c -7 "$repo/.git/HEAD")" # unnamed commit
-			tag="$(fgrep -rl "$branch" "$repo/.git/refs/tags")" # is this a tag?
-			if [ -f "$repo/.git/packed-refs" ]; then # aggressively packed
-				tag="$tag$(grep "$branch.*refs/tags" "$repo/.git/packed-refs")"
+			branch="$(cut -c -7 "$git_dir/HEAD")" # unnamed commit
+			tag="$(fgrep -rl "$branch" "$git_dir/refs/tags")" # is this a tag?
+			if [ -f "$git_dir/packed-refs" ]; then # aggressively packed
+				tag="$tag$(grep "$branch.*refs/tags" "$git_dir/packed-refs")"
 			fi
 			[ ! -z "$tag" ] && branch="${tag##*/}";;
 		No) # no branches exist or branch named 'No'
-			[ -z "$(ls "$repo/.git/refs/heads")" ] &&
-			[ ! -f "$repo/.git/packed-refs" ] && branch="(init)" || branch="$f";;
+			[ -z "$(ls "$git_dir/refs/heads")" ] &&
+			[ ! -f "$git_dir/packed-refs" ] && branch="(init)" || branch="$f";;
 		*) # normal mode
 			branch="$f" # obtain upstream info if available
 			for g in $(echo "$info" | tail -n +3); do
@@ -46,7 +50,7 @@ state="$(echo "$data" | tail -n +2 | cut -c -2)"
 [ ! -z "$(echo "$state" | cut -c 2 | tr -d '? ')" ] && color="$dirty" && bits="$bits*"
 echo "$state" | grep -q '?' && color="$dirty" && bits="$bits%" # untracked files
 echo "$state" | grep -q 'U' && branch="<!>$branch" # merge conflict
-[ -f "$repo/.git/refs/stash" ] && bits="^$bits" # stash exists
+[ -f "$git_dir/refs/stash" ] && bits="^$bits" # stash exists
 
 # parse color options
 if [ ! -z "$1" ]; then
