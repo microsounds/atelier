13ca9291b70c100f102ecdcda85e2ac93bb90568
microsounds <microsounds@users.noreply.github.com>
2020-04-10 20:42:57 -0700

    Purging old unused scripts from ~2015 or so

--- Scripts/old/common.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Most common out of $(cat ~/.bash_history | wc -l) commands:"
-cat ~/.bash_history | sed 's/sudo //g' | cut -d ' ' -f1 | sort | uniq -c | sort -nr | head -20
