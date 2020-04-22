6f0b467741aaf84c919a3ad8cd714cd56b10e1f6
microsounds <microsounds@users.noreply.github.com>
2020-04-22 15:16:08 -0700

    dwm_status: Removed useless loop, fixed other issues
    Variable reuse
    Skip rewriting approximate time on error
    Workaround for blank values

--- Scripts/dwm_status.sh
+++ Scripts/dwm_status.sh
@@ -2,7 +2,7 @@
 # dwm status bar
 # provides formatted system information
 
-_='・' # separator
+_='・' # separator character
 fan_data='/proc/acpi/ibm/fan' # thinkpad acpi
 
 # -p for extra padding
@@ -20,31 +20,36 @@ if [ ! -z "$TEMP" ]; then
 		[ ! $FAN -eq 0 ] && FAN="Fan ${FAN}%" || FAN="$TEMP" # fan off
 	fi
 fi
+
 # networking
 NET="$(nmcli | fgrep 'connected' | sed 's/connected to //' | head -1)"
 [ ! -z "$NET" ] || NET="Network Off"
+
 # power management
 acpi="$(acpi -b | egrep -o '[0-9]+\%.*')"
 pct="$(echo "$acpi" | egrep -o '[0-9]+%' | head -1)"
-if [ $pct != '100%' ]; then
-	for btime in "$(echo "$acpi" | egrep -o '([0-9]+:?)+' | tail -1)"; do
-		i=0; for f in h m; do # rewrite time remaining as approximation
-			i=$((i + 1))
-			val="$(echo "$btime" | cut -d ':' -f$i | sed 's/^0//')"
-			[ ! $val -eq 0 ] && btime_v="$btime_v$val$f"
-		done
-		for f in $acpi; do case $f in
-			charged) BAT="$btime_v till charged";;
-			remaining) BAT="$btime_v left";;
-		esac; done
+
+# ignore blank, 'Unknown' or 'Charging at zero rate' values
+if [ "$acpi" != "$pct" ] && ! echo "$acpi" | egrep -q '(Unknown|zero)'; then
+	btime="$(echo "$acpi" | egrep -o '([0-9]+:?)+' | tail -1)"
+	i=0; for f in h m; do # rewrite time remaining as approximation
+		i=$((i + 1))
+		val="$(echo "$btime" | cut -d ':' -f$i | sed 's/^0//')"
+		[ ! $val -eq 0 ] && btime_v="$btime_v$val$f"
 	done
+	for f in $acpi; do case $f in
+		charged) btime_v="$btime_v till charged";;
+		remaining) btime_v="$btime_v left";;
+	esac; done
 fi
-BAT="Bat $pct${BAT:+, $BAT}"
+BAT="Bat $pct${btime_v:+, $btime_v}"
+
 # audio
 sound="$(amixer get 'Master')"
 VOL="Vol $(echo "$sound" | egrep -o '[0-9]+%' | head -1)"
 [ ! -z "$(echo "$sound" | fgrep 'off')" ] && VOL="$VOL, muted"
-# time
+
+# time/date
 TIME="$(date '+%-l:%M%P')"
 for day in $(date '+%-e'); do
 	case $day in
@@ -55,4 +60,5 @@ for day in $(date '+%-e'); do
 	esac
 done
 DATE="$(date '+%a, %b') $day"
+
 echo "${PAD}${FAN:+${FAN}$_}${NET}$_${BAT}$_${VOL}$_${DATE}$_${TIME}${PAD}"
