8b19e704c0a86b4cb3a155be45b6a8d3750cc8c2
microsounds <microsounds@users.noreply.github.com>
2020-05-03 21:34:30 -0700

    Almost switched to stterm

--- .once.d/00-dwm-install.sh
+++ .once.d/00-dwm-install.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
 
-# dwm install script
-# configuration should already exist in ~/.config/dwm
-# git refuses to clone into non-empty directories and git-submodule is a mess
-
-CONFIG="$HOME/.config/dwm"
-ORIGIN='git://git.suckless.org/dwm'
-VERSION='6.2'
+# automated dwm install script
+# configuration should already exist in ~/.config
 
 echo "$0"
-[ ! -d "$CONFIG" ] && echo "'$CONFIG' doesn't exist." && exit 1
 
-if cd "$CONFIG" && ! git status 2> /dev/null; then
-	git init
-	git remote add origin "$ORIGIN"
-	git fetch --tags origin master
-fi
-if git checkout -f "$VERSION"; then
-	for f in patches/*; do # apply patches
-		patch < $f;
-	done
-	make install PREFIX="$HOME/.local" -j $(grep -c '^proc' /proc/cpuinfo)
-fi
+for f in dwm-6.2; do
+	VERSION="${f#*-}"
+	CONFIG="$HOME/.config/${f%-*}"
+	ORIGIN="git://git.suckless.org/${f%-*}"
+	[ ! -d "$CONFIG" ] && echo "'$CONFIG' doesn't exist." && exit 1
+
+	if cd "$CONFIG" && ! git status 2> /dev/null; then
+		git init
+		git remote add origin "$ORIGIN"
+		git fetch --tags origin master
+	fi
+	if git checkout -f "$VERSION"; then
+		# apply patches
+		[ ! -d patches ] || for g in patches/*; do patch < $g; done
+		make install PREFIX="$HOME/.local" -j $(grep -c '^proc' /proc/cpuinfo)
+	fi
+done
