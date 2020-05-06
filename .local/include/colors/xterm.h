96218eff5c850ce4669ed36b5656f869b4417688
microsounds <microsounds@users.noreply.github.com>
2020-05-05 17:38:59 -0700

    Fallback colors

---
+++ .local/include/colors/xterm.h
@@ -0,0 +1,45 @@
+#ifndef XTERM_H
+#define XTERM_H
+
+#define str(s) _xstr(s)
+#define _xstr(s) #s
+
+/* special */
+#define FGCOLOR	#FFFFFF
+#define FGLIGHT	#FFFFFF
+#define BGCOLOR	#000000
+#define BGLIGHT	#000000
+
+/* black */
+#define COLOR0	#000000
+#define COLOR8	#808080
+
+/* red */
+#define COLOR1	#800000
+#define COLOR9	#FF0000
+
+/* green */
+#define COLOR2	#008000
+#define COLOR10	#00FF00
+
+/* yellow */
+#define COLOR3	#808000
+#define COLOR11	#FFFF00
+
+/* blue */
+#define COLOR4	#000080
+#define COLOR12	#0000FF
+
+/* magenta */
+#define COLOR5	#800080
+#define COLOR13	#FF00FF
+
+/* cyan */
+#define COLOR6	#008080
+#define COLOR14	#00FFFF
+
+/* white */
+#define COLOR7	#C0C0C0
+#define COLOR15	#FFFFFF
+
+#endif /* XTERM_H */
