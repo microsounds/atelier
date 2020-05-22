9e5c1400b3ff9828c67c1683f2624d35312d066b
microsounds <microsounds@users.noreply.github.com>
2020-05-22 12:39:26 -0700

    Nothing important

--- .once.d/21-x86-mitigations.sh
+++ .once.d/21-x86-mitigations.sh
@@ -7,6 +7,7 @@ KEY='GRUB_CMDLINE_LINUX_DEFAULT'
 OPTION='quiet mitigations=off'
 
 echo "$0"
+# append required key if it doesn't exist
 grep -q "$KEY" "$CONF" || echo "$KEY" | sudo tee -a "$CONF"
 sudo sed -i "/.*$KEY/c $KEY=\"$OPTION\"" "$CONF"
 sudo update-grub
--- .once.d/23-chromebook-delete-key.sh
+++ .once.d/23-chromebook-delete-key.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# chromebook hack
+# systemd-logind chromebook hack
 # disables power key so it can be remapped as Delete/F11
 
 read vendor < /sys/devices/virtual/dmi/id/sys_vendor
@@ -11,5 +11,7 @@ KEY='HandlePowerKey'
 OPTION='ignore'
 
 echo "$0"
+# append required key if it doesn't exist
 grep -q "$KEY" "$CONF" || echo "$KEY" | sudo tee -a "$CONF"
 sudo sed -i "/.*$KEY/c $KEY=$OPTION" "$CONF"
+echo "You must restart logind for changes to take effect."
