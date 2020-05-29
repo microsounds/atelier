5c25a11c502ba01543fac2c1f747d157a92ca990
microsounds <microsounds@users.noreply.github.com>
2020-05-29 15:48:54 -0700

    dwm_status: Enable wired connection information

---
+++ .once.d/20-network-manager.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# allow network-manager to manage wired devices
+# that appear in /etc/network/interfaces
+
+CONF='/etc/NetworkManager/NetworkManager.conf'
+KEY='managed'
+OPTION='true'
+
+echo "$0"
+# append required key if it doesn't exist
+grep -q "$KEY" "$CONF" || echo "$KEY" | sudo tee -a "$CONF"
+sed "/.*$KEY/c $KEY=$OPTION" < "$CONF" | sudo tee "$CONF"
+sudo systemctl restart network-manager
