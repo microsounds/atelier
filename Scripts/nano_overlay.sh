e824b014e21f71d14c6fb3a5a2c9d58f3cbbac8e
microsounds <microsounds@users.noreply.github.com>
2020-06-03 01:13:08 -0700

    nano_overlay: Quit if file or temp directory aren't writeable

--- Scripts/nano_overlay.sh
+++ Scripts/nano_overlay.sh
@@ -6,6 +6,18 @@
 
 mesg_st() { printf '%s%s' "${mode:+[$mode] }" "$@"; } # for prompts
 mesg() { mesg_st "$@"; printf '\n'; }
+quit() { mesg "$@, exiting."; exit 1; }
+
+derive_parent() (
+	# return parent dir if path has
+	# ../relative/sub/dirs or is absolute path
+	# return '.' if path is in the current dir
+	if echo "$@" | fgrep -q '/'; then
+		path="${@%/*}"
+		path="${path:-/}" # nothing left, assume '/'
+	fi
+	echo "${path:-.}"
+)
 
 mode_help() {
 	nano "$@"
@@ -22,8 +34,7 @@ mode_help() {
 
 jump_to() {
 	if [ "$(echo "$1" | cut -f3 | cut -c 1-2)" = '/^' ]; then
-		mesg 'ctags must be run in '-n' mode for numeric line numbers.'
-		exit 1
+		quit 'ctags must be run in '-n' mode for numeric line numbers'
 	fi
 	file="$(echo "$1" | cut -f2)"
 	line="$(echo "$1" | cut -f3 | egrep -o '[0-9]+')"
@@ -35,18 +46,15 @@ mode_ctags() {
 	# find root directory containing ctags index
 	# all filenames are relative to this directory
 	while [ ! -z "$PWD" ] && [ ! -f "$PWD/tags" ]; do PWD="${PWD%/*}"; done
-	if [ -z "$PWD" ]; then
-		mesg 'No index found in current or any parent directories up to /.'
-		exit 1
-	fi
-	[ -z "$1" ] && mesg 'Enter a tag query.' && exit 1
+	[ -z "$PWD" ] && quit 'No index found in this or any parent directories up to /'
+	[ -z "$1" ] && quit 'No tag query given'
 
 	# find matches based on first column
 	for f in $(cut -f1 < "$PWD/tags" | grep -i -n "$1" | cut -d ':' -f1); do
 		list="$list${f}p;"
 	done
 	matches="$(sed -n "$list" < "$PWD/tags")"
-	[ -z "$matches" ] && mesg "No matches found for $1." && exit 1
+	[ -z "$matches" ] && quit "No matches found for $1"
 
 	num="$(echo "$matches" | wc -l)"
 	if [ "$num" -gt 1 ]; then # multiple matches
@@ -73,18 +81,21 @@ mode_ctags() {
 
 random_bits() { tr -cd 'a-z0-9' < /dev/urandom | head -c $1; }
 
-get_pass() {
-	stty -echo # impure function
-	read -r pass && printf '\n'
+get_response() {
+	stty -echo
+	read -r res
 	stty echo
+	echo "$res"
 }
 
 prompt_user() {
-	read -r res # expect a 'yes' response
-	case $res in
-		y | Y | yes) return 0;;
-		*) return 1
-	esac
+	while read -r res; do
+		case "$(echo "$res" | tr 'A-Z' 'a-z')" in
+			y | yes) return 0;;
+			n | no) return 1;;
+		esac
+		mesg_st "Please confirm (yes/no): "
+	done
 }
 
 mode_encrypt() {
@@ -92,8 +103,17 @@ mode_encrypt() {
 	# global settings
 	magic='openssl'
 	cipher='-aes-256-cbc -pbkdf2'
+	# temp file directory
+	prefix="${XDG_RUNTIME_DIR:-/tmp}"
+
 	for f in "$@"; do
-		while :; do tmp="/tmp/${f##*/}.$(random_bits 7)"
+		# file permissions
+		for g in "$prefix" "$(derive_parent "$f")"; do
+			[ ! -w "$g" ] && quit "'$g' is unwritable"
+		done
+
+		# randomize filename
+		while :; do tmp="$prefix/${f##*/}.$(random_bits 7)"
 			[ -f "$tmp" ] || break
 		done
 
@@ -102,23 +122,20 @@ mode_encrypt() {
 		[ -f "$f" ] && file -b "$f" | grep -q "^$magic" && state='encrypted'
 		# no state - file is plaintext, ask to overwrite when finished
 
-		mesg_st "Password for '$f'${state:+ ($state)}: " && get_pass
+		mesg_st "Password for '$f'${state:+ ($state)}: "
+		pass="$(get_response)" && printf '\n'
 		if [ "$state" != 'encrypted' ]; then # verify password
 			orig="$pass"
-			mesg_st "Verify password: " && get_pass
-			if [ "$orig" != "$pass" ]; then
-				mesg "Passwords do not match, exiting."
-				exit 1
-			fi
+			mesg_st 'Verify password: '
+			pass="$(get_response)" && printf '\n'
+			[ "$orig" != "$pass" ] && quit 'Passwords do not match'
 			unset orig
 		fi
 
 		trap '[ ! -f "$tmp" ] || shred -z -u "$tmp"' 0 1 2 3 6
-		if [ "$state" = 'encrypted' ]; then
+		if [ "$state" = 'encrypted' ]; then # decrypt file
 			if ! openssl enc $cipher -pass "pass:$pass" -d < "$f" | xz -d > "$tmp"; then
-				mesg 'Invalid password, exiting.'
-				rm -rf "$tmp"
-				exit 1
+				quit 'Invalid password'
 			fi
 			init="$(sha256sum < "$tmp")" # monitor changes
 		fi
@@ -129,7 +146,7 @@ mode_encrypt() {
 
 		if [ -f "$tmp" ]; then
 			if [ -z "$state" ]; then # ask to overwrite original
-				mesg_st "Overwrite original file '$f'? (y/Y/yes): "
+				mesg_st "Overwrite original file '$f'? (yes/no): "
 				prompt_user && state='ok'
 			fi
 			if [ ! -z "$state" ] && [ "$init" != "$(sha256sum < "$tmp")" ]; then
@@ -154,12 +171,10 @@ if [ ! -z "$1" ]; then # steal options not supported by GNU nano
 		done
 	fi
 	for f in "$@"; do # force quit on lockfiles and other mistakes
-		# path has ../relative/sub/dirs or is absolute
-		echo "$f" | fgrep -q '/' && bname="${f%/*}"
-		if [ -f "${bname:-.}/.${f##*/}.swp" ]; then
-			mesg "'$f' already in use, exiting." && exit 1
+		if [ -f "$(derive_parent "$f")/.${f##*/}.swp" ]; then
+			quit "'$f' already in use"
 		elif [ -d "$f" ]; then
-			mesg "'$f' is a directory, exiting." && exit 1
+			quit "'$f' is a directory"
 		fi
 	done
 fi
