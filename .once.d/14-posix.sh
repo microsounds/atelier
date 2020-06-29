5865068a7f5ae3d720337196f488f6498559e9e4
microsounds <microsounds@users.noreply.github.com>
2020-06-29 00:09:29 -0700

    Download POSIX/SUSv4-2018 standards docs

---
+++ .once.d/14-posix.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# downlond POSIX/SUSv4-2018 standards docs
+
+SOURCE='https://pubs.opengroup.org/onlinepubs/9699919799/download'
+BINARY='susv4-2018.tar.bz2'
+INSTALL="$HOME/.local/share/doc"
+
+echo "$0"
+mkdir -pv "$INSTALL"
+wget -q -O - "$SOURCE/$BINARY" | tar -xvj -C "$INSTALL"
+mv -v "$INSTALL/${BINARY%%.*}" "$INSTALL/posix"
