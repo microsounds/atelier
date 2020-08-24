a99e0ee8f822a9dcdb6b7b83c5c17b01582f8767
microsounds <microsounds@users.noreply.github.com>
2020-08-24 15:32:52 -0700

    Set upstream without pushing during post-install

--- .once.d/02-meta-config.sh
+++ .once.d/02-meta-config.sh
@@ -4,9 +4,10 @@
 echo "$0"
 
 # upstream
+git meta remote remove origin
 git meta remote add origin 'ssh://git@github.com/microsounds/atelier'
-git meta push -u origin master
+git meta fetch
+git meta branch -u origin/master
 
 # set to ignore all untracked files in $HOME when calling git status
 git meta config status.showUntrackedFiles no
-
