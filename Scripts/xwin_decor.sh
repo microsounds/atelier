31cae66bbdd59084806fc62b4987c4c3a7b3a776
microsounds <microsounds@users.noreply.github.com>
2020-08-25 13:21:13 -0700

    dwm: Allow spaces in wallpaper filenames

--- Scripts/xwin_decor.sh
+++ Scripts/xwin_decor.sh
@@ -3,14 +3,11 @@
 # decorate root window
 # use bitmap background as fallback
 
-position='--bg-fill -g +0+0' # top of wallpaper
-images="$HOME/Pictures/active"
-bitmaps="$HOME/.local/share/X11/bitmaps"
-
 # xload
 ~/Scripts/xload.sh vert bottom-right &
 
-# fallback wallpaper
+# fallback tiling background
+bitmaps="$HOME/.local/share/X11/bitmaps"
 cpp <<- EOF | egrep -v '^(#|$)' | while read -r color; do
 	#include <colors/nightdrive.h>
 	-bg str(COLOR15) -fg str(COLOR1)
@@ -18,5 +15,7 @@ EOF
 	echo "$color" | xargs -o xsetroot -bitmap "$bitmaps/diag.xbm"
 done
 
-# wallpaper
-find "$images" -type f | shuf | head -n 1 | xargs feh --no-fehbg $position || exit
+# select random wallpaper
+images="$HOME/Pictures/active"
+sel="$(find "$images" -type f | shuf | head -n 1)"
+feh --no-fehbg --bg-fill -g '+0+0' "$sel" || exit
