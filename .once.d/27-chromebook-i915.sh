f0691ff5503330af45b96ba36dda42a9a7cb4ed4
microsounds <microsounds@users.noreply.github.com>
2020-09-22 01:49:08 -0700

    i915 stability fix for defective chromebook hardware

---
+++ .once.d/27-chromebook-i915.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# chromebook i915 stability fix
+# some extremely poorly-binned (read: defective) chromebook hardware can
+# exhibit stability issues with the i915 display driver when making use of
+# hardware acceleration
+# eg. jumpy pointer, keyboard repeated inputs, system hanging and freezing
+# this can be fixed by disabling panel-self refresh in i915
+
+read vendor < /sys/devices/virtual/dmi/id/sys_vendor
+[ "$vendor" != 'GOOGLE' ] && exit 0
+
+echo "$0"
+sudo tee '/etc/modprobe.d/i915.conf' <<- EOF
+	# disable panel-self refresh to mitigate system hangs and instability
+	options i915 enable_psr=0
+EOF
+
+
