3d25b8e8a5e6b4cfe48ade8a42bfa40714f7722a
microsounds <microsounds@users.noreply.github.com>
2020-11-01 20:18:16 -0800

    Post-install: Fetch latest Realtek drivers

--- .once.d/22-realtek-rtl8812au.sh
+++ .once.d/22-realtek-rtl8812au.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-# sideload a DKMS module from Kali Linux for Realtek RTL8812AU-based wifi cards
+# install DKMS module for Realtek RTL8812AU-based wifi cards
+# sideloaded from Kali Linux
 
 TMP="/tmp/$(tr -cd 'a-z0-9' < /dev/urandom | dd bs=7 count=1 2> /dev/null)"
 SOURCE='https://http.kali.org/kali/pool/contrib/r/realtek-rtl88xxau-dkms'
-DEB='realtek-rtl88xxau-dkms_5.6.4.2~20200529-0kali1_all.deb'
+DEB="$(wget -q -O - "$SOURCE" | egrep -o '<a href=".*\.deb">' | \
+	tr '"' "\t" | cut -f2 | tail -n 1)"
 
 finish() {
 	rm -rv "$TMP"
@@ -16,6 +18,7 @@ trap finish 0 1 2 3 6
 
 echo "$0"
 mkdir -v "$TMP"
+echo "Fetching '$DEB'..."
 if wget "$SOURCE/$DEB" -O "$TMP/$DEB"; then
 	sudo dpkg -i "$TMP/$DEB"
 fi
