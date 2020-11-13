753dbbaea5c9bd84387c001866b0d09bdbdd475f
microsounds <microsounds@users.noreply.github.com>
2020-11-12 22:35:22 -0800

    Documentation

--- .once.d/29-intel-undervolt.sh
+++ .once.d/29-intel-undervolt.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 
 # install optional intel-undervolt daemon and systemd service
-# a 100mV undervolt should be stable enough for Core i7 systems with terrible thermals
+# works only with 4th-gen Haswell or newer architectures
 # undervolt daemon must still be configured and enabled manually with systemctl
+
+# Usage Notes
+# -100mV is usually enough to control thermal throttling or runaway thermals
+# on aging 2012-2018 hyper-threaded Intel desktop CPUs mfd. with non-soldered
+# heat spreaders that, over time, degrade their thermal performance at
+# sustained full load unless you delid or increase physical cooler size.
 
 TMP="/tmp/$(tr -cd 'a-z0-9' < /dev/urandom | dd bs=7 count=1 2> /dev/null)"
 REPO='https://github.com/kitsunyan/intel-undervolt'
