bf5cb33a98f0880e3cf48d195a94263a6f3ca546
microsounds <microsounds@users.noreply.github.com>
2020-11-22 00:18:26 -0800

    Post-install: Return zero if virt-manager is not installed

--- .once.d/28-libvirt-rootless.sh
+++ .once.d/28-libvirt-rootless.sh
@@ -3,4 +3,5 @@
 # bypass polkit in virt-manager
 # permanently authenticates the current user
 
-dpkg -s 'virt-manager' > /dev/null 2>&1 && sudo adduser $(whoami) libvirt
+dpkg -s 'virt-manager' > /dev/null 2>&1 || exit 0
+sudo adduser $(whoami) libvirt
