b1b56da8c366317db4c54b69834e0568adced4e3
microsounds <microsounds@users.noreply.github.com>
2020-11-22 01:32:35 -0800

    Post-install: Install Realtek drivers only if hardware is installed

--- .once.d/22-realtek-rtl8812au.sh
+++ .once.d/22-realtek-rtl8812au.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
-# install DKMS module for Realtek RTL8812AU-based wifi cards
-# sideloaded from Kali Linux
+# install Linux drivers for known Realtek RTL8812AU-based wifi devices
+# sideload DKMS module packaged by Kali Linux maintainers
+for f in \
+	'2357:011e' # TP-Link Archer T2U Nano AC600 Wireless USB Adapter
+do lsusb -d "$f" && found=1; done
+[ ! -z "$found" ] || exit 0
 
 TMP="/tmp/$(tr -cd 'a-z0-9' < /dev/urandom | dd bs=7 count=1 2> /dev/null)"
 SOURCE='https://http.kali.org/kali/pool/contrib/r/realtek-rtl88xxau-dkms'
