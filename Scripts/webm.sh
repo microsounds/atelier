59abf55990bad6e5289487a55b5eecc3ca13b273
microsounds <microsounds@users.noreply.github.com>
2021-01-01 22:00:31 -0800

    webm.sh: Prevent filename word splitting, write temporary files in $PWD

--- Scripts/webm.sh
+++ Scripts/webm.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
 ## webm.sh v0.5
-#  screen recorder, outputs soundless webm for certain anime imageboards
-## Press Ctrl+C to end recording, press Ctrl+C again to cancel.
+## Press Ctrl+C to stop recording, press Ctrl+C again to cancel.
+
+# simple screen recorder
+# outputs soundless vp8 webm as required by certain anime imageboards
 
 # colors
 INFO=37
 OK=32
 ERR=31
 
-# quality
+# quality settings
 SIZE=2.8M
 CONSTQ=31
 BITRATE=3M # 4M
@@ -17,12 +19,19 @@ SCALE=-1:-1 # 800:-1
 FPS=60 # 25
 
 # system
+KEY="$(tr -cd 'a-z0-9' < /dev/urandom | dd bs=10 count=1 2> /dev/null)"
+RES="$(xdpyinfo | grep 'dim' | egrep -o '([0-9]+x?)+' | head -n 1)"
 NAME="${0##*/}" # derive script name
 CORES="$(grep -c '^proc' /proc/cpuinfo)"
-RES="$(xdpyinfo | grep 'dim' | egrep -o '([0-9]+x?)+' | head -n 1)"
-FINAL="$(date '+%Y-%m-%d-%H%M%S')_${RES}_${NAME%.*}.webm"
-KEY="$(tr -cd 'a-z0-9' < /dev/urandom | dd bs=10 count=1 2> /dev/null)"
-TEMP="/tmp/${KEY}.mp4"
+
+# final encode settings (2-pass)
+ENC_OPTS="-hide_banner -loglevel info -c:v libvpx \
+	-b:v $BITRATE -crf $CONSTQ -fs $SIZE -vf scale=$SCALE \
+	-fs $SIZE -threads $CORES -an -passlogfile $KEY"
+
+# filenames
+TEMP="$PWD/${KEY}.mp4"
+FINAL="$PWD/$(date '+%Y-%m-%d-%H%M%S')_${RES}_${NAME%.*}.webm"
 
 info() { echo "\e[1;${1}m${2}\e[0m"; }
 
@@ -32,22 +41,19 @@ to_webm() {
 		for f in $(seq 2); do
 			info $INFO "Pass $f encoding..."
 			case $f in
-				1) OUTPUT='-f null /dev/null';;
-				2) OUTPUT="$PWD/$FINAL";;
+				1) ffmpeg -i "$TEMP" $ENC_OPTS -pass $f -f null /dev/null;;
+				2) ffmpeg -i "$TEMP" $ENC_OPTS -pass $f "$FINAL";;
 			esac
-			ffmpeg -hide_banner -loglevel info -i "$TEMP" -c:v libvpx \
-				-b:v $BITRATE -crf $CONSTQ -fs $SIZE -vf scale=$SCALE \
-				-fs $SIZE -threads $CORES -an -passlogfile $KEY -pass $f $OUTPUT
 		done
-		rm -v "$TEMP" "$PWD/$KEY"*
-		info $OK "File saved at: $PWD/$FINAL"
+		rm -fv "$PWD/$KEY"*
+		info $OK "File saved at: $FINAL"
 	else
 		info $ERR "Terminated abruptly..."
-		rm -v "$TEMP" "$PWD/$KEY"* "$FINAL" && exit 1
+		rm -fv "$PWD/$KEY"* "$FINAL" && exit 1
 	fi
 }
 
 info $INFO "$(cat $0 | grep '^##' | sed 's/## //g')"
 iter=0; trap to_webm 2
 ffmpeg -loglevel panic -threads $CORES -framerate $FPS -video_size $RES \
-       -f x11grab -i :0.0+0,0 -vcodec libx264 -qp 0 -preset ultrafast "$TEMP"
+       -f x11grab -i :0.0+0,0 -c:v libx264 -qp 0 -preset ultrafast "$TEMP"
