4d653b98eee27e188927d8627952cce2082d63f4
microsounds <microsounds@users.noreply.github.com>
2021-03-24 21:09:42 -0700

    Post-install: Set to push through ssh only

--- .once.d/02-meta-config.sh
+++ .once.d/02-meta-config.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env sh
 
-# git meta configuration automated setup
-
-# use ssh by default, fallback to https if ~/.ssh doesn't exist
-prefix='ssh://git@'
-[ ! -d ~/.ssh ] && prefix='https://'
+# git meta upstream configuration
 upstream='github.com/microsounds/atelier'
+ssh='ssh://git@'
+https='https://'
 
-# fetch upstream
+# set to push only through ssh
 git meta remote remove origin
-git meta remote add origin "$prefix$upstream"
+git meta remote add origin "$https$upstream"
+git meta remote set-url origin --push "$ssh$upstream"
 git meta fetch || exit 1
 git meta branch -u origin/master
 
