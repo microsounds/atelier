afd45694432c065de78294a8720fc2ce71d3c1c3
microsounds <microsounds@users.noreply.github.com>
2021-04-18 02:17:56 +0000

    Termux: Nested subshell hack, remove splash screen

--- .once.d/a0-android-termux.sh
+++ .once.d/a0-android-termux.sh
@@ -7,7 +7,15 @@ uname -o | tr 'A-Z' 'a-z' | fgrep -q 'android' || exit 0
 pkg install -y proot git openssl-tool openssh bash-completion
 
 # allow use of standard file locations like /tmp
-! fgrep -q 'termux-chroot' < ~/.profile && echo "termux-chroot" >> ~/.profile
+# termux loads .bashrc before .profile
+! fgrep -q 'termux-chroot' < ~/.bashrc && \
+	sed '1s/^/[ $SHLVL -lt 2 ] \&\& termux-chroot \&\& exit\n/' -i ~/.bashrc
 
 # allow use of stdin on devices without root
 find ~/Scripts ~/.local -type f | xargs sed -e 's,/dev/stdin,/proc/self/fd/0,g' -i
+
+# remove splash screen
+rm -f /etc/motd
+
+# setup dotfile upstream
+~/.once.d/02-meta-config.sh
