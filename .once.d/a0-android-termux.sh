6b7778b9fb73eda3a1593f62714f194371471ddb
microsounds <microsounds@users.noreply.github.com>
2021-04-17 23:57:04 -0700

    Termux: Search and replace file descriptors for non-rooted android

--- .once.d/a0-android-termux.sh
+++ .once.d/a0-android-termux.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env sh
 
-# collection of hackjobs to enable basic functionality on android termux
+# collection of hackjobs to enable basic functionality on termux for android
+# these changes are incompatible with standards compliant *NIX-likes
+# and will be applied manually instead of added as edge cases within scripts
 
 uname -o | tr 'A-Z' 'a-z' | fgrep -q 'android' || exit 0
 
 pkg install -y proot git openssl-tool openssh bash-completion
 
 # allow use of standard file locations like /tmp
-# termux loads .bashrc before .profile
-! fgrep -q 'termux-chroot' < ~/.bashrc && \
-	sed '1s/^/[ $SHLVL -lt 2 ] \&\& termux-chroot \&\& exit\n/' -i ~/.bashrc
+! fgrep -q 'termux-chroot' < ~/.profile && \
+	sed '1s/^/termux-chroot\n/' -i ~/.profile
 
-# allow use of stdin on devices without root
-find ~/Scripts ~/.local -type f | xargs sed -e 's,/dev/stdin,/proc/self/fd/0,g' -i
+# allow use of standard file descriptors on devices without root
+fd=0; for f in stdin stdout stderr; do
+	script="$script;s,/dev/$f,/proc/self/fd/$fd,g"
+	fd=$((fd + 1))
+done
+find ~/Scripts ~/.local -type f | xargs sed -e "$script" -i
 
 # remove splash screen
 rm -f /etc/motd
