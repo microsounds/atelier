43d6631f75d57c61a86ad8b7b303d9f62025709d
microsounds <microsounds@users.noreply.github.com>
2021-04-22 05:07:01 +0000

    Termux: Automatically generate color scheme and softkey layout

--- .once.d/a0-android-termux.sh
+++ .once.d/a0-android-termux.sh
@@ -6,7 +6,10 @@
 
 uname -o | tr 'A-Z' 'a-z' | fgrep -q 'android' || exit 0
 
-pkg install -y proot git openssl-tool openssh bash-completion
+mkdir -p ~/.termux
+
+# install prerequisites
+pkg install -y proot git openssl-tool openssh clang bash-completion
 
 # allow use of standard file locations like /tmp
 ! fgrep -q 'termux-chroot' < ~/.profile && \
@@ -22,5 +25,28 @@ find ~/Scripts ~/.local -type f | xargs sed -e "$script" -i
 # remove splash screen
 rm -f /etc/motd
 
-# setup dotfile upstream
+# reset meta upstream
 ~/.once.d/02-meta-config.sh
+
+# strip unsupported nano features
+sed -e '/scroller/d' -i ~/.nanorc
+
+# import color scheme
+colors='#include <colors/nightdrive.h>
+cursor=FGCOLOR
+foreground=FGCOLOR
+background=BGCOLOR'
+for f in $(seq 0 15); do
+	colors="$colors
+color$f=COLOR$f"
+done
+echo "$colors" | cpp -P > ~/.termux/colors.properties
+
+# configure soft keys
+cat <<- EOF > ~/.termux/termux.properties
+	extra-keys-style = arrows-all
+	extra-keys = [ \
+		['ESC','~','/','HOME','UP','END','PGUP','DEL'], \
+		['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN','BKSP'] \
+	]
+EOF
