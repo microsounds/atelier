cb64c6220828281fb1e93c9ab6a2100034ee68ce
microsounds <microsounds@users.noreply.github.com>
2021-04-22 15:36:20 -0700

    Termux: Automatically install fonts

--- .once.d/a0-android-termux.sh
+++ .once.d/a0-android-termux.sh
@@ -2,14 +2,12 @@
 
 # collection of hackjobs to enable basic functionality on termux for android
 # these changes are incompatible with standards compliant *NIX-likes
-# and will be applied manually instead of added as edge cases within scripts
+# and will be applied manually so as to not pollute other scripts
 
 uname -o | tr 'A-Z' 'a-z' | fgrep -q 'android' || exit 0
 
-mkdir -p ~/.termux
-
 # install prerequisites
-pkg install -y proot git openssl-tool openssh clang bash-completion
+pkg install -y wget git proot clang openssl-tool openssh bash-completion
 
 # allow use of standard file locations like /tmp
 ! fgrep -q 'termux-chroot' < ~/.profile && \
@@ -28,8 +26,23 @@ rm -f /etc/motd
 # reset meta upstream
 ~/.once.d/02-meta-config.sh
 
-# strip unsupported nano features
+# strip unsupported .nanorc flags
 sed -e '/scroller/d' -i ~/.nanorc
+
+# termux-specific
+# install fonts
+CONF="$HOME/.termux"
+SOURCE='https://packages.debian.org/sid/all/fonts-go/download'
+SOURCE="$(wget -q -O - "$SOURCE" | egrep -o '<a href=.*>' \
+	| tr '"' '\t' | cut -f2 | grep 'deb$' | head -n 1)"
+DEB="${SOURCE##*/}"
+
+mkdir -p "$CONF"
+if wget "$SOURCE" -O "$CONF/$DEB" || exit 1; then
+	ar -p "$CONF/$DEB" 'data.tar.xz' | xz -d \
+		| tar -xO --wildcards '*Go-Mono.ttf' > "$CONF/font.ttf"
+	rm -f "$CONF/$DEB"
+fi
 
 # import color scheme
 colors='#include <colors/nightdrive.h>
@@ -40,13 +53,13 @@ for f in $(seq 0 15); do
 	colors="$colors
 color$f=COLOR$f"
 done
-echo "$colors" | cpp -P > ~/.termux/colors.properties
+echo "$colors" | cpp -P > "$CONF/colors.properties"
 
-# configure soft keys
-cat <<- EOF > ~/.termux/termux.properties
+# configure soft key layout
+cat <<- EOF > "$CONF/termux.properties"
 	extra-keys-style = arrows-all
 	extra-keys = [ \
-		['ESC','~','/','HOME','UP','END','PGUP','DEL'], \
-		['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN','BKSP'] \
+		[ ESC, '~',  '/', HOME, UP,   END,   PGUP, DEL  ], \
+		[ TAB, CTRL, ALT, LEFT, DOWN, RIGHT, PGDN, BKSP ] \
 	]
 EOF
