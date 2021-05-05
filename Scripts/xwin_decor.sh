45f4a5662a046744b457bd974add3d63b94187cb
microsounds <microsounds@users.noreply.github.com>
2021-05-05 14:23:33 -0700

    xwin_decor: Use random second instead of % value to cat video frames

--- Scripts/xwin_decor.sh
+++ Scripts/xwin_decor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# xwin_decor.sh v0.7
+# xwin_decor.sh v0.8
 # decorate root window
 
 # fallback tiling background
@@ -16,37 +16,46 @@ rand() {
 	{ od -N 4 -t u -A n | tr -d ' '; } < /dev/urandom
 }
 
+shuffle() {
+	input="$(cat /dev/stdin)"
+	lines=$(echo "$input" | wc -l)
+	echo "$input" | tail -n +$((($(rand) % lines) + 1)) | head -n 1
+}
+
+ffmpeg_cat() {
+	mediainfo "$1" --inform='Video;%FrameCount% %FrameRate%' \
+		| while read -r f_count fps; do
+		secs=$(echo "scale=2; $f_count * (1 / $fps)" | bc)
+		ffmpeg -ss $(($(rand) % ${secs%.*})) -i "$1" -vframes 1 \
+			-q:v 0 -f image2pipe -vcodec png - 2> /dev/null
+	done
+}
+
 temp="$(mk-tempdir)" && mkdir -p "$temp"
 trap 'rm -rf "$temp"' 0 1 2 3 6
 
 config="$HOME/.xdecor"
 [ -f "$config" ] || exit
-lines=$(wc -l) < "$config"
 
 # iterate through all active displays
 xrandr -q | fgrep '*' | while read -r dpy; do
 	# randomly select directory
-	{ grep . | tail -n $((($(rand) % lines) + 1)) | head -n 1; } < "$config" \
-		| while read -r dir; do
+	{ grep . | shuffle; } < "$config" | while read -r dir; do
 		[ "${dir%${dir#?}}" = '~' ] && dir="$HOME/${dir#??}" # absolute path
 		[ ! -z "$dir" ] || exit
 
 		# randomly select file
 		find "$dir" -type f \
-			| egrep '\.(jpe?g|png|mkv|mp4|web(m|p))$' | shuf -n 1 \
-			| while read -r sel; do
+			| egrep '\.(jpe?g|png|mkv|mp4|web(m|p))$' \
+			| shuffle | while read -r sel; do
 			[ ! -z "$sel" ] || exit
 
 			case "$sel" in
-				*mkv|*mp4|*webm) # video file
-					while seed=$(($(rand) % 100)); do # exclude OP/EDs
-						[ $seed -lt 15 ] || [ $seed -gt 85 ] || break
-					done
-					ffmpegthumbnailer -i "$sel" -s 0 -c png -t $seed -o - ;;
+				*mkv|*mp4|*webm) ffmpeg_cat "$sel";; # video file
 				*) cat "$sel"
 			esac
-		done
-	done > "$temp/$(rand)"
+		done > "$temp/$(rand)"
+	done
 done
 
 find "$temp" -type f | xargs feh --no-fehbg --bg-fill -g +0+0
