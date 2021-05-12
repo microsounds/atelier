d183a0f004881a54de473dfd5682ae419ea7c13a
microsounds <microsounds@users.noreply.github.com>
2021-05-12 13:03:09 -0700

    network-manager: ifupdown still required

--- .once.d/21-network-manager.sh
+++ .once.d/21-network-manager.sh
@@ -8,11 +8,10 @@
 # don't wait on networking during startup
 sudo systemctl disable NetworkManager-wait-online.service
 
-# FIXME: NetworkManager in bullseye seems to do this by default now
-# allow network-manager to manage wired devices from /etc/network/interfaces
+# allow NetworkManager to manage wired devices from /etc/network/interfaces
 CONF='/etc/NetworkManager/NetworkManager.conf'
 KEY='managed'
-OPTION='false'
+OPTION='true'
 
 # append required key if it doesn't exist
 grep -q "$KEY" < "$CONF" || echo "$KEY" | sudo tee -a "$CONF"
