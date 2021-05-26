4d22501b6c403f2a0ab448a27299c0e35620656a
microsounds <microsounds@users.noreply.github.com>
2021-05-26 16:01:48 -0700

    Post-install: Automatically designate hibernation disk

--- .once.d/23-grub-config.sh
+++ .once.d/23-grub-config.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
+# grub configuration
+# designate swap space for hibernation
 # disable x86 mitigations
-
-CONF='/etc/default/grub'
-KEY='GRUB_CMDLINE_LINUX_DEFAULT'
-OPTION='quiet mitigations=off'
 
 # not needed during unit testing
 ! is-container || exit 0
+
+CONF='/etc/default/grub'
+KEY='GRUB_CMDLINE_LINUX_DEFAULT'
+
+UUID="$(/usr/sbin/blkid | grep 'swap' | head -n 1 | tr ' ' '\n' | grep '^UUID')"
+OPTION="resume=$UUID loglevel=0 mitigations=off"
 
 # append required key if it doesn't exist
 grep -q "$KEY" < "$CONF" || echo "$KEY" | sudo tee -a "$CONF"
