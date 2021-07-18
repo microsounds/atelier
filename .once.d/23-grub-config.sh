0eaf079a9287481b544caa089950377bdeb2a7a6
microsounds <microsounds@users.noreply.github.com>
2021-07-17 19:41:18 -0700

    Allow daemons that use MSR writes to continue working under Linux 5.x

--- .once.d/23-grub-config.sh
+++ .once.d/23-grub-config.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 # grub configuration
-# designate swap space for hibernation
-# disable x86 mitigations
+# append linux kernel tweaks
 
 # not needed during unit testing
 ! is-container || exit 0
@@ -11,7 +10,12 @@ CONF='/etc/default/grub'
 KEY='GRUB_CMDLINE_LINUX_DEFAULT'
 
 UUID="$(sudo blkid | grep 'swap' | head -n 1 | tr ' ' '\n' | grep '^UUID')"
-OPTION="resume=$UUID loglevel=0 mitigations=off"
+
+# designate swap space for suspend to disk
+# ignore chatty startup text
+# disable meltdown/spectre mitigations
+# allow undervolt/overclock from userspace
+OPTION="resume=$UUID loglevel=0 mitigations=off msr.allow_writes=on"
 
 conf-append "$KEY=$OPTION" "$CONF"
 sudo update-grub
