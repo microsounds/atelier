d1d71863cd291024b19d06d8e8600cf6e569c9e6
microsounds <microsounds@users.noreply.github.com>
2021-07-24 15:03:35 -0700

    Post-install: Limit size of journald logs

--- .once.d/26-systemd-tweaks.sh
+++ .once.d/26-systemd-tweaks.sh
@@ -7,9 +7,13 @@ for f in Start Stop; do
 	conf-append "DefaultTimeout${f}Sec=10s" '/etc/systemd/system.conf'
 done
 
+# limit size of journald logs
+conf-append 'SystemMaxUse=20M' '/etc/systemd/journald.conf'
+
 # convert hybrid-sleep to act as hibernate+reboot
 conf-append 'HybridSleepMode=reboot' '/etc/systemd/sleep.conf'
 conf-append 'HibernateDelaySec=0s' '/etc/systemd/sleep.conf'
 
 # allow unprivileged users to view kernel syslog
 conf-append 'kernel.dmesg_restrict = 0' '/etc/sysctl.conf'
+
