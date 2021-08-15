93c8e1f8aef49d4e4bc0e3e86c187dbdc0e06c1c
microsounds <microsounds@users.noreply.github.com>
2021-08-15 15:13:38 -0700

    Post-install: POSIX documentation is inaccessible for some reason

--- .once.d/13-posix-docs.sh
+++ .once.d/13-posix-docs.sh
@@ -2,9 +2,13 @@
 
 # download POSIX/SUSv4-2018 standards docs
 
+# 2021/08: Downloads inaccessible from the original site, using wayback machine
+ARCHIVE='https://web.archive.org/web/20180412190321'
+
 SOURCE='https://pubs.opengroup.org/onlinepubs/9699919799/download'
-BINARY='susv4-2018.tar.bz2'
+BINARY='susv4-2018.tgz'
 INSTALL="$HOME/.local/share/doc"
 
 mkdir -pv "$INSTALL"
-wget -q -O - "$SOURCE/$BINARY" | bzip2 -d | tar -xv -C "$INSTALL" || exit 1
+wget -q -O - "$ARCHIVE/$SOURCE/$BINARY" \
+	| gzip -d | tar -xv -C "$INSTALL" || exit 1
