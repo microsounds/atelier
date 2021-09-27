369d38c711759764ba873ac4661b2b4173107026
microsounds <microsounds@users.noreply.github.com>
2021-09-27 10:51:39 -0700

    Documentation

--- .once.d/c1-chromebook-i915.sh
+++ .once.d/c1-chromebook-i915.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
-# chromebook i915 stability fix
+# chromebook i915 stability fix on intel bay trail devices
+# see https://en.wikipedia.org/wiki/Silvermont#Erratum
+#
 # some extremely poorly-binned (read: defective) chromebook hardware can
 # exhibit stability issues with the i915 display driver when making use of
 # hardware acceleration
 # eg. jumpy pointer, keyboard repeated inputs, system hanging and freezing
-# this can be fixed by disabling panel-self refresh in i915
+# this can be mostly avoided by disabling panel-self refresh in i915
 
 ! is-chromebook && exit 0
 
