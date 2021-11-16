504ad66e42dc348318ee5c00f9e118cd3881b8f2
microsounds <microsounds@users.noreply.github.com>
2021-11-15 23:00:59 -0800

    Tweaked light theme colors

--- .local/include/colors/overcast.h
+++ .local/include/colors/overcast.h
@@ -1,7 +1,7 @@
 #ifndef OVERCAST_H
 #define OVERCAST_H
 
-/* overcast rainy v.1 ~microsounds */
+/* overcast rainy v.2 ~microsounds */
 
 /* special */
 #define FGCOLOR	#4D4D4C
@@ -14,12 +14,12 @@
 #define COLOR8	#5D6F74
 
 /* red */
-#define COLOR1	#D5A8E3
-#define COLOR9	#CD749C
+#define COLOR1	#CD749C
+#define COLOR9	#CF659F
 
 /* green */
-#define COLOR2	#9C75DD
-#define COLOR10	#63B0B0
+#define COLOR2	#6C48A3
+#define COLOR10	#00796B
 
 /* yellow */
 #define COLOR3	#9898AE
