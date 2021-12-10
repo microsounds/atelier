c65893efa655e041e303527327f30428243de7fb
microsounds <microsounds@users.noreply.github.com>
2021-12-10 01:52:51 -0800

    xwin-widgets: Rewrote display res detection, added Xinerama support

--- Scripts/xwin_widgets.sh
+++ Scripts/xwin_widgets.sh
@@ -1,20 +1,49 @@
 #!/usr/bin/env sh
 
-# xwin_widgets.sh v0.3
+# xwin_widgets.sh v0.4
 # launch xload and xclock in a corner somewhere
-# dowm ignore EMWH resize hints in tiling mode
+# dwm ignores EMWH resize hints in tiling mode
 # start in floating mode or override widgets to always float
 
 # constants
 LIST='xload xclock'
 for f in $LIST; do COUNT=$((COUNT + 1)); done # calculate distance
 
-# xdotool cannot span across multiple connected displays
-SCREEN=$(xrandr -q | grep '[^dis]connected' | egrep -o '([0-9]+x?)+' \
-	| fgrep 'x' | head -n 1)
-SIZE=130  # window size
-GAP=15    # gap between windows
-EDGE=20   # distance from screen border
+# Xinerama support hacks
+# xdotool windowmove commands cannot span across a multi-monitor Xinerama
+# desktop. windowmove commands are bound to the dimensions of the monitor that
+# window is currently on, this is considered the "active monitor". windowmove
+# commands beyond those dimensions will silently fail. However, xdotool has no
+# way of indicating the dimensions of the active monitor, which is required to
+# generate correct x,y offsets for the spawned widgets.
+
+# widgets will appear on the monitor that currently has the pointer, nudge
+# pointer to the right to ensure pointer and widgets are on the same monitor
+xdotool mousemove_relative --sync 1 0 click 1
+
+# pointer location, provides MOUSE_XOFF, MOUSE_YOFF
+eval "$(xdotool getmouselocation | sed -E 's/([a-z]+)/mouse_\1off/g' \
+	| tr ':' '=' | tr 'a-z' 'A-Z' | tr ' ' '\n' | head -n 2)"
+
+# detect display resolution by guessing which monitor has the pointer
+# xrandr display dimension format: 'WIDTHxHEIGHT+x_off+y_off'
+for f in $(xrandr -q | grep '[^dis]connected' \
+	| egrep -o '([0-9]+x?\+?)+' | fgrep 'x'); do
+	# dimensions
+	dim="${f%%+*}"; WIDTH="${dim%x*}"; HEIGHT="${dim#*x}"
+	# +x, +y offsets
+	off="${f#$dim}"; x_off="${off%+*}"; y_off="${off#$x_off}"
+	# strip leading symbols
+	x_off="${x_off#?}"; y_off="${y_off#?}"
+	# take a wild guess
+	[ $MOUSE_XOFF -lt $((WIDTH + x_off)) ] && \
+		[ $MOUSE_XOFF -gt $WIDTH ] && break
+	[ $MOUSE_YOFF -lt $((HEIGHT + y_off)) ] && \
+		[ $MOUSE_YOFF -gt $HEIGHT ] && break
+done
+SIZE=130      # window size
+GAP=15        # gap between windows
+EDGE=20       # distance from screen border
 
 # dumb parser
 # calculates starting positions
@@ -22,10 +51,10 @@ ARGS="${@:-vert bottom right}" # defaults
 echo "$ARGS" | fgrep -q 'vert' && VERT=1 # vertical flag
 for f in $(echo "$ARGS" | sed 's/-/ /g'); do
 	case $f in # (display res - edge) - ((winsize + gap) * # of windows)
-		left)           XPOS=$EDGE;;
-		upper | top)    YPOS=$EDGE;;
-		right)          XPOS=$(( (${SCREEN%x*} - EDGE) - ((SIZE + GAP) * ${VERT-$COUNT}) ));;
-		bottom | lower) YPOS=$(( (${SCREEN#*x} - EDGE) - (SIZE + GAP) ));;
+		left) XPOS=$EDGE;;
+		upper|top) YPOS=$EDGE;;
+		right) XPOS=$(( (WIDTH - EDGE) - ((SIZE + GAP) * ${VERT-$COUNT}) ));;
+		bottom|lower) YPOS=$(( (HEIGHT - EDGE) - (SIZE + GAP) ));;
 	esac
 done
 
