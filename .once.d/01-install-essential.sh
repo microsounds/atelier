e4bf0defa9640e449b8926666542fa1a7871ccd8
microsounds <microsounds@users.noreply.github.com>
2022-03-11 22:34:37 -0800

    Post-install: Simplified checking for build prereqs with is-installed
    - Package checking has been moved into it's own library function.
    - Installing build prereqs for dwm/dmenu have been moved out of ~/.comforts
      and moved into a shared pre-run script.

--- .once.d/01-install-essential.sh
+++ .once.d/01-install-essential.sh
@@ -14,13 +14,8 @@ for f in $(cat ~/.comforts | sed 's/#.*$//g'); do
 		\*) # package groups with an asterisk are optional, prompt user
 			f="${f#?}"
 			# don't ask if package group is fully installed
-			unset IFS not_inst
-			for g in $f; do
-				printf '%s\r' "Checking '$g'..."
-				dpkg -s "$g" > /dev/null 2>&1 || { not_inst=1; break; }
-			done
-			[ -z "$not_inst" ] && continue
-			user-confirm "Install optional package(s) $f?" || continue;;
+			is-installed $f > /dev/null ||
+				user-confirm "Install optional package(s) $f?" || continue
 	esac
 	pkgs="$pkgs $f"
 done
