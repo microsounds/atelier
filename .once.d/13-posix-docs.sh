d9275c0d97983885f2350533931f0f65acc7049d
microsounds <microsounds@users.noreply.github.com>
2023-01-20 11:59:21 -0800

    Nothing important, post-install validation is now on a timer
    Also downloading POSIX docs from official source again.

--- .once.d/13-posix-docs.sh
+++ .once.d/13-posix-docs.sh
@@ -3,7 +3,7 @@
 # download POSIX/SUSv4-2018 standards docs
 
 # 2021/08: Downloads inaccessible from the original site, using wayback machine
-ARCHIVE='https://web.archive.org/web/20180412190321'
+#ARCHIVE='https://web.archive.org/web/20180412190321'
 
 SOURCE='https://pubs.opengroup.org/onlinepubs/9699919799/download'
 BINARY='susv4-2018.tgz'
@@ -15,5 +15,5 @@ mkdir -pv "$INSTALL_DIR"
 grep -qr "$VERSION" "$INSTALL_DIR" && \
 	echo "$VERSION already installed." && exit 0
 
-wget -q -O - "$ARCHIVE/$SOURCE/$BINARY" \
+wget -q -O - "${ARCHIVE:+$ARCHIVE}$SOURCE/$BINARY" \
 	| gzip -d | tar -xv -C "$INSTALL_DIR" || exit 1
