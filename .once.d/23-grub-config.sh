f03d4a5cc7c8a9664f8e166b6568b1e65bbc2d3c
microsounds <microsounds@users.noreply.github.com>
2023-08-26 09:24:17 -0700

    grub: Re-enabling running os-prober by default for dual-boot setups

--- .once.d/23-grub-config.sh
+++ .once.d/23-grub-config.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
 # grub configuration
-# append linux kernel tweaks
 
 CONF='/etc/default/grub'
-KEY='GRUB_CMDLINE_LINUX_DEFAULT'
 
 # not needed during unit testing
 ! is-container || exit 0
@@ -12,13 +10,17 @@ KEY='GRUB_CMDLINE_LINUX_DEFAULT'
 # probably an ARM device using u-boot
 [ -f "$CONF" ] || exit 0
 
+# append linux kernel tweaks
 unset OPTION
+KEY='GRUB_CMDLINE_LINUX_DEFAULT'
 # ignore chatty startup text
 OPTION="$OPTION loglevel=0"
 # disable meltdown/spectre mitigations
 OPTION="$OPTION mitigations=off"
 # allow undervolt/overclock from userspace
 OPTION="$OPTION msr.allow_writes=on"
-
 conf-append "$KEY=$OPTION" "$CONF"
+
+# dual-boot: enable os-prober by default
+conf-append "GRUB_DISABLE_OS_PROBER=false" "$CONF"
 sudo update-grub
