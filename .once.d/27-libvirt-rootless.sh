ebedc9331c3f5ebd382ebadb9e0c04694aa217b9
microsounds <microsounds@users.noreply.github.com>
2025-08-01 12:59:53 -0700

    virt-manager: Enable 'default' network at boot

--- .once.d/27-libvirt-rootless.sh
+++ .once.d/27-libvirt-rootless.sh
@@ -2,6 +2,9 @@
 
 # bypass polkit in virt-manager
 # permanently authenticates the current user
-
 dpkg -s 'virt-manager' > /dev/null 2>&1 || exit 0
 sudo adduser $(whoami) libvirt
+
+# persistent networking across reboots
+sudo virsh net-start default
+sudo virsh net-autostart default
