1c46ad2d9f60f6daf3edce30193141a7963b2b0b
microsounds <microsounds@users.noreply.github.com>
2026-03-28 12:01:19 -0700

    fontconfig: Added optional compile-time HiDPI setting for HiDPI devices
    5/???: Changes for convertible tablet PC usage

--- .local/include/theme.h
+++ .local/include/theme.h
@@ -2,33 +2,25 @@
 #define THEME_H
 
 /*
- * Notes on theming and display issues
+ * Notes on HiDPI and readability
  *
- * Fallback glyphs and FN_EMOJI (future)
- * Emoji glyphs are currently provided by fallback font DejaVu Sans in
- * all applications, future changes to Debian might necessitate introducing a
- * subset of DejaVu Sans or another font with just emoji glyphs to
- * selectively override FN_{TERM,TEXT,HEADER}, generated during post-install.
-
- * Future changes required for HiDPI
- * fontconfig boilerplate must be changed to relative name:size=px
- * FN_TERM changes to size 10, FN_HEADER changes to size 12
- * xwin-widgets will have to apply multiplers based on reported DPI
+ * fontconfig font sizes are defined in 'pixelsize' by default
+ * Readability optimized for low density displays up to 1080p @ 18" or greater
  *
- * Major problems with HiDPI
- * Xft.dpi value in ~/.xresources is respected mostly everywhere
- *  - x pointer has an absolute pixelsize and is unusable without overrides
- *  - certain emoji glyphs in dwm are absolutely tiny and unusable
- *  - border widths in dwm are defined in pixel widths and also practically invisible
- *  - HiDPI support is lacking in many unexpected places, not going to bother.
+ * OPTIONAL: define HIDPI_MODE on systems with high density displays,
+ *           eg. 4K displays below 24", 1080p @ 13" or smaller.
+ * Control display density setting via 'Xft.dpi' in ~/.xresources
+ * You must recompile dwm/dmenu for changes to take effect.
+ * Restart X to apply to all other applications respecting ~/.xresources
  */
+#undef HIDPI_MODE
 
 /* utilities and fontconfig boilerplate */
 #define _xstr(s) #s
 #define str(s) _xstr(s)
 #define font(name, px) name:pixelsize=px
 
-/* font size in pixel height */
+/* font size in pixelsize */
 #define FN_TERM        Go Mono
 #define FN_TERM_JP     VL Gothic
 #define FN_TERM_SIZE   13
@@ -42,5 +34,37 @@
 
 #define FN_EMOJI       DejaVu Sans
 #define FN_EMOJI_SIZE  16
+
+#ifdef HIDPI_MODE
+/* 	font size in relative size */
+	#define font(name, px) name:size=px
+	#define FN_TERM_SIZE	10
+	#define FN_TEXT_SIZE	9
+	#define FN_HEADER_SIZE	12
+	#define FN_EMOJI_SIZE	12
+#endif
+
+/*
+ * Notes on theming and display issues
+ *
+ * Fallback glyphs and FN_EMOJI (future)
+ * Emoji glyphs are currently provided by fallback font DejaVu Sans in
+ * all applications, future changes to Debian might necessitate introducing a
+ * subset of DejaVu Sans or another font with just emoji glyphs to
+ * selectively override FN_{TERM,TEXT,HEADER}, generated during post-install.
+
+ * Changes requires for HiDPI
+ * fontconfig boilerplate must be changed to relative name:size=px
+ * FN_TERM changes to size 10, FN_HEADER changes to size 12
+ * xwin-widgets will have to apply multiplers based on reported DPI
+ *
+ * Major problems with HiDPI
+ * HiDPI is global, you cannot mix displays with widely different DPI densities
+ * Xft.dpi value in ~/.xresources is respected mostly everywhere
+ *  - x pointer has an absolute pixelsize and is unusable without overrides
+ *  - certain emoji glyphs in dwm are absolutely tiny and unusable
+ *  - border widths in dwm are defined in pixel widths and also practically invisible
+ *  - HiDPI support is lacking in many unexpected places, not going to bother.
+ */
 
 #endif /* THEME_H */
