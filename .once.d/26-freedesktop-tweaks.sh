2052c55b98804f4fe9d2d983164ccdf9081cfb77
microsounds <microsounds@users.noreply.github.com>
2026-07-18 23:58:01 -0700

    bluetooth: Sideload non-free AAC codecs for supported bluetooth devices
    This is gross, but compiling debian's libspa-0.2-bluetooth package from deb-src would be even worse.
    Also silencing chatty otd-daemon output at startup.

--- .once.d/26-freedesktop-tweaks.sh
+++ .once.d/26-freedesktop-tweaks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# collection of systemd/freedesktop-related tweaks
+# snowballing collection of systemd/freedesktop-related tweaks
 
 # limit systemd start/stop job timers to 10 seconds
 for f in Start Stop; do
@@ -24,11 +24,25 @@ conf-append 'kernel.dmesg_restrict = 0' '/etc/sysctl.conf'
 # xdg-desktop-portal-gtk hangs pavucontrol, OBS, and other apps
 yes y | sudo apt-get autoremove --purge gnome-keyring* xdg-desktop-portal*
 
+# install symlink to locally maintained system-sleep hooks
+sudo ln -sfv \
+	"$(realpath ~/.local/lib/systemd-sleep)" '/lib/systemd/system-sleep'
+
 # ensure pulseaudio isn't installed on machine
 systemctl --user enable pipewire pipewire-pulse
 systemctl --user disable pulseaudio.service pulseaudio.socket
 yes y | sudo apt-get autoremove --purge pulseaudio
 
-# install symlink to locally maintained system-sleep hooks
-sudo ln -sfv \
-	"$(realpath ~/.local/lib/systemd-sleep)" '/lib/systemd/system-sleep'
+# sideloads non-free AAC codec support for supported bluetooth audio devices,
+# deliberately excluded from libspa-0.2-bluetooth on Debian 12+
+# See 'https://tookmund.com/2024/02/aac-and-debian' and
+# 'https://blog.fernvenue.com/archives/airpods-pro-2-on-debian/' for more info.
+
+SOURCE='https://blog.fernvenue.com/others/libspa-codec-bluez5-aac_0.3.65_amd64.deb'
+TEMP="$(mk-tempdir)"
+
+wget -O "$TEMP" "$SOURCE"
+sudo dpkg -i "$TEMP"
+rm -rf "$TEMP"
+systemctl --user restart wireplumber pipewire pipewire-pulse | :
+
